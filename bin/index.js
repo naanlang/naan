a8cd075f281154b74e5a41ec4d4fdbf9836ec8ac
Richard Zulch <richard@zulchlabs.com>
2023-02-11 18:58:57 -0800

    Changes for v1.0.9+1: 1. Added a language checker to find semantic defects such as duplicate procedure names within a common parent procedure, unused local variables, duplicate `local declaration, invalid or duplicate parameters, break or continue tags that do not exist in an enclosing loop, break or continue outside a loop, and dead code that will never be executed. 2. The language checker allows declaration of global references, with any non-declared references causing an error. This is currently opt-in by including "global(...)" at the top of a procedure. 3. Fixed all the defects in the codebase detected by the new language checker. 4. Added unit tests for the language checker. 5. Implemented template strings using ${<expression>} syntax, with $${ being an escape if the literal characters are desired. Template substitution occurs after adjacent strings are concatenated. Templates are active in strings with both "" and '' delimiters. 6. Added string character element subscripting with . and [] operators. Index >= 0 selects the nth character, while an index < 0 selects the nth character from the end of the string, so "abc".-1 returns "c". String.charAt is used, so iterating through Unicode characters outside the basical multilingual plane will require other arrangements.

--- bin/index.js
+++ bin/index.js
@@ -58,11 +58,11 @@ process.argv.every((val, index) => {
         return (true);
     }
     if (val == "--version") {
-        console.log("1.0.8");
+        console.log("1.0.9");
         process.exit(0);
     }
     if (val == "--buildno") {
-        console.log("1.0.8+1");
+        console.log("1.0.9+1");
         process.exit(0);
     }
     if (val.substring(0,1) == "-") {
