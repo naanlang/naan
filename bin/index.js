8a40194bcdb85efea775e7fd53ce742df2586d78
Richard Zulch <richard@zulchlabs.com>
2023-09-06 20:15:36 +0300

    Changes since 1.0.13: 1. Frameworks/browser/HttpsApiRequest now returns status as third element of tuple, even with error. 2. Frameworks/browser/HttpsApiRequest also returns status, and offers SSL options. 3. Fixed various bugs in the builder, including a hang mode with errors in catenate components. 4. Fixed various schema bugs in the OpenSearch plugin, including related to the currency type 5. For DynamoDB, added support for exists() and attribute paths to conditional expressions, fixed bug where getRecord did not propagate the rangeValue, and now supports consistent reads. 6. Fixed a long-standing interpreter bug where exporting a Naan object to JavaScript could recurse infinitely. 7. Improved error handling in the copyann() builtin. 8. Cleaned up REPL prompt and welcome message handling. E.g. an uncaught exception no longer disables the prompt. 9. Fixed an interpreter bug where evaluating a literal dictionary would assign undefined to a key instead of omitting it as intended. 10. Modified apply() to work like call() to accept all procedure types instead of just native Naan procedures. 11. Added ability to delete keys in merge(). Dictionary arguments are merged, while tuple arguments are taken as a list of keys to be deleted. 12. Fixed a long-standing bug where Array([5]) would display as [Array 5] instead of [Array [5]], which arose from Array.toString helpfully removing all bracket levels from its output. 13. Improved Lingo parser error handling to require that ambiguous newline expression splits utilize an explicit ',' to separate the lines or '\' to join them. Also detects and reports stray "else" keywords. 14. Added over 100 additional unit tests to verify the changes above.

--- bin/index.js
+++ bin/index.js
@@ -58,11 +58,11 @@ process.argv.every((val, index) => {
         return (true);
     }
     if (val == "--version") {
-        console.log("1.0.13");
+        console.log("1.0.14");
         process.exit(0);
     }
     if (val == "--buildno") {
-        console.log("1.0.13+1");
+        console.log("1.0.14+1");
         process.exit(0);
     }
     if (val.substring(0,1) == "-") {
