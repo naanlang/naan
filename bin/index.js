117191206a021f68971c85987c84dc4a7ba82182
Richard Zulch <richard@zulchlabs.com>
2024-08-05 19:46:42 -0700

    Changes since 1.2.0: - Added SSL support for NaanServer - Improved remote evaluation - Many bug fixes - Added unit tests

--- bin/index.js
+++ bin/index.js
@@ -58,11 +58,11 @@ process.argv.every((val, index) => {
         return (true);
     }
     if (val == "--version") {
-        console.log("1.2.0");
+        console.log("1.2.1");
         process.exit(0);
     }
     if (val == "--buildno") {
-        console.log("1.2.0+1");
+        console.log("1.2.1+1");
         process.exit(0);
     }
     if (val.substring(0,1) == "-") {
